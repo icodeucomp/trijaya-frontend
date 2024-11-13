96da9b995ee67df678e1fbc23165ddf9f8d227ec
helmyfadlail <helmyfadlail.5@gmail.com>
2024-11-13 18:58:18 +0700

    refactor: change api prefix name

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -7,7 +7,7 @@ interface AxiosProps {
   options?: AxiosRequestConfig;
 }
 
-export const baseUrlApi = "https://tbm-backend-dot-icodeu-431422.et.r.appspot.com/api/v1";
+export const baseUrlApi = "https://trijaya-berkah-mandiri.et.r.appspot.com/api/v1";
 
 const axiosInstance = axios.create({ baseURL: baseUrlApi });
 
